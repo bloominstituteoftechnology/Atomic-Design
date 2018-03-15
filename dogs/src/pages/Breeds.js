314539405bc9a7e5e57b22c67c8b22abe5eb2067
Abraham Andujo <foxtara00@gmail.com>
2018-03-15 16:00:24 -0700

    Abraham Andujo Complete

--- dogs/src/pages/Breeds.js
+++ dogs/src/pages/Breeds.js
@@ -16,10 +16,39 @@ class Breeds extends Component {
       terrier: "terrier",
       poodle: "poodle",
       setter: "setter"
-    }
+    };
 
     return breeds.filter(breed => choiceBreeds[breed]);
   }
+
+
+  componentDidMount() {
+    axios.get('https://dog.ceo/api/breeds/list/all')
+    .then(response => {
+      console.log(response);
+      this.setState({ breeds: this.filterBreeds(Object.keys(response.data.message)).map(breed => {
+            return { path: `subbreeds/${breed}`, label: breed };
+          }
+        )
+      });
+      axios.get(`https://dog.ceo/api/breed/${this.state.breeds[0].label}/images/random`)
+        .then(response => {
+          console.log("Getting Image: ", response);
+          this.setState({ imgLabel: this.state.breeds[0].label, imgUrl: response.data.message });
+        });
+    });
+  }
+
+  render() {
+    return (
+      <Options
+        title="Breeds"
+        list={this.state.breeds}
+        imgLabel={this.state.imgLabel}
+        imgUrl={this.state.imgUrl}
+      />
+    );
+  } 
 }
 
 export default Breeds;
