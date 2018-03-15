33b02b4b0beed89ab9474439da7be02bdf282803
noxasaxon <saxon.h@outlook.com>
2018-03-15 15:49:58 -0700

    subBreeds populated

--- dogs/src/pages/SubBreeds.js
+++ dogs/src/pages/SubBreeds.js
@@ -14,14 +14,34 @@ class SubBreeds extends Component {
     axios
       .get(`https://dog.ceo/api/breed/${this.props.match.params.breed}/list`)
       .then(response => {
-        console.log(response);
         this.setState({
-          subBreeds: response.data.message
+          subBreeds: response.data.message,
+          imgLabel: response.data.message[0]
         });
+        for (let i = 0; i < this.state.subBreeds.length; i++) {
+          this.state.subBreeds[i] = {
+            label: this.state.subBreeds[i]
+          };
+        }
+        console.log(response.data.message[0]);
+        axios
+          .get(`https://dog.ceo/api/breed/hound/afghan/images/random`)
+          .then(response => {
+            this.setState({
+              imgUrl: response.data.message
+            });
+          });
       });
   }
   render() {
-    return <div />;
+    return (
+      <Options
+        title="Sub Breeds"
+        list={this.state.subBreeds}
+        imgUrl={this.state.imgUrl}
+        imgLabel={this.state.imgLabel}
+      />
+    );
   }
 }
 
