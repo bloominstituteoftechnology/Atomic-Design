e850893880b2937be8a6e39735e6d9784460dc76
ivanmora <iam.ivan.mora@gmail.com>
2018-03-15 11:09:37 -0500

    Create Assignment without page code

---
+++ dogs/src/pages/Breeds.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Options } from '../templates';
+
+class Breeds extends Component {
+  state = {
+    breeds: [],
+    imgLabel: "",
+    imgUrl: ""
+  }
+
+  filterBreeds(breeds) {
+    const choiceBreeds = {
+      hound: "hound",
+      retriever: "retriever",
+      terrier: "terrier",
+      poodle: "poodle",
+      setter: "setter"
+    }
+
+    return breeds.filter(breed => choiceBreeds[breed]);
+  }
+}
+
+export default Breeds;
---
+++ dogs/src/pages/SubBreeds.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Options } from '../templates';
+
+class SubBreeds extends Component {
+  state = {
+    subBreeds: [],
+    imgLabel: "",
+    imgUrl: ""
+  }
+}
+
+export default SubBreeds;
