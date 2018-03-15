0f3fa45b304c0596232c7783ce4a562b471f00da
Matthew Tomas <matthewebentomas@gmail.com>
2018-03-15 19:48:02 -0400

    breed & subbreed list showing

--- dogs/src/pages/Breeds.js
+++ dogs/src/pages/Breeds.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+
+import { Link } from '../atoms';
 import { Options } from '../templates';
 
 class Breeds extends Component {
@@ -21,8 +23,24 @@ class Breeds extends Component {
     return breeds.filter(breed => choiceBreeds[breed]);
   }
 
+  componentDidMount() {
+    axios.get("https://dog.ceo/api/breeds/list/all").then(response => {this.setState({
+          breeds: this.filterBreeds(Object.keys(response.data.message)).map(
+            breed => {return { path: `subbreeds/${breed}`, label: breed };})});});
+  }
+
   render() {
-    return <div><p>Hello from Breeds.js render</p></div>
+    return (
+      <div>
+        <p>Hello from Breeds.js render</p>
+        <Options
+        title="Breeds"
+        list={this.state.breeds}
+        imgLabel={this.state.imgLabel}
+        imgUrl={this.state.imgUrl}
+        />
+      </div>
+    );
   }
 }
 
--- dogs/src/pages/SubBreeds.js
+++ dogs/src/pages/SubBreeds.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+
+import { List } from '../molecules';
 import { Options } from '../templates';
 
 class SubBreeds extends Component {
@@ -7,6 +9,32 @@ class SubBreeds extends Component {
     subBreeds: [],
     imgLabel: "",
     imgUrl: ""
+  }
+
+  componentDidMount() {
+    axios.get(`https://dog.ceo/api/breed/${this.props.match.params.breed}/list`)
+      .then(response => {
+        this.setState({
+          subBreeds: response.data.message.map(breed => {
+            return { label: breed };
+          }
+        )
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello from SubBreeds.js render</p>
+        <Options
+          title={this.props.match.params.breed}
+          list={this.state.subBreeds}
+          imgLabel={this.state.imgLabel}
+          imgUrl={this.state.imgUrl}
+        />
+      </div>
+    );
   }
 }
 
