beecfd2daed6db2c8df1c79bf2f28bd494c6409f
kait-schorr <kaitlynschorr@gmail.com>
2018-03-15 14:46:57 -0500

    Got layour and images working

--- dogs/src/pages/Breeds.js
+++ dogs/src/pages/Breeds.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Options } from '../templates';
+import React, { Component } from "react";
+import axios from "axios";
+import { Options } from "../templates";
+import { Link } from "../atoms";
 
 class Breeds extends Component {
   state = {
     breeds: [],
     imgLabel: "",
     imgUrl: ""
-  }
+  };
 
   filterBreeds(breeds) {
     const choiceBreeds = {
@@ -16,9 +17,44 @@ class Breeds extends Component {
       terrier: "terrier",
       poodle: "poodle",
       setter: "setter"
-    }
-
+    };
     return breeds.filter(breed => choiceBreeds[breed]);
+  }
+  componentDidMount() {
+    axios.get("https://dog.ceo/api/breeds/list/all").then(response => {
+      console.log(response);
+      this.setState({
+        breeds: this.filterBreeds(Object.keys(response.data.message)).map(
+          breed => {
+            return { path: `subbreeds/${breed}`, label: breed };
+          }
+        )
+      });
+      axios
+        .get(
+          `https://dog.ceo/api/breed/${
+            this.state.breeds[0].label
+          }/images/random`
+        )
+        .then(response => {
+          console.log("Getting Image: ", response);
+          this.setState({
+            imgLabel: this.state.breeds[0].label,
+            imgUrl: response.data.message
+          });
+        });
+    });
+  }
+
+  render() {
+    return (
+      <Options
+        title="Breeds"
+        list={this.state.breeds}
+        imgLabel={this.state.imgLabel}
+        imgUrl={this.state.imgUrl}
+      />
+    );
   }
 }
 
--- dogs/src/pages/SubBreeds.js
+++ dogs/src/pages/SubBreeds.js
@@ -1,12 +1,46 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Options } from '../templates';
+import React, { Component } from "react";
+import axios from "axios";
+import { Options } from "../templates";
+import { List } from "../molecules";
 
 class SubBreeds extends Component {
   state = {
     subBreeds: [],
     imgLabel: "",
     imgUrl: ""
+  };
+  render() {
+    return (
+      <Options
+        title={this.props.match.params.breed}
+        list={this.state.subBreeds}
+        imgLabel={this.state.imgLabel}
+        imgUrl={this.state.imgUrl}
+      />
+    );
+  }
+  componentDidMount() {
+    axios
+      .get(`https://dog.ceo/api/breed/${this.props.match.params.breed}/list`)
+      .then(response => {
+        this.setState({
+          subBreeds: response.data.message.map(breed => {
+            return { label: breed };
+          })
+        });
+        axios
+          .get(
+            `https://dog.ceo/api/breed/${this.props.match.params.breed}/${
+              this.state.subBreeds[0].label
+            }/images/random`
+          )
+          .then(response => {
+            this.setState({
+              imgLabel: this.state.subBreeds[0].label,
+              imgUrl: response.data.message
+            });
+          });
+      });
   }
 }
 
