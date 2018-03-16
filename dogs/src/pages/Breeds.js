86cfe19594c9d16c5a415117b116331e68f2be49
mjd02 <mostdarryl@gmail.com>
2018-03-16 19:07:59 -0400

    base functionality of breeds working

--- dogs/src/pages/Breeds.js
+++ dogs/src/pages/Breeds.js
@@ -9,6 +9,27 @@ class Breeds extends Component {
     imgUrl: ""
   }
 
+  componentDidMount(){
+    axios.get('https://dog.ceo/api/breeds/list').then( (res)=>{
+      res.data.message.forEach( (e,i)=>{
+        if(i === 0){
+          axios.get('https://dog.ceo/api/breed/'+e+'/images').then( (res)=>{
+            this.setState({imgUrl:res.data.message[0]});
+            console.log(res);
+
+          }).catch( (err)=>{
+            console.log(err);
+          });
+        }
+        let breeds = this.state.breeds.slice();
+        breeds.push(e); 
+        this.setState({breeds:breeds});
+      });
+    }).catch( (err)=>{
+      console.log(err);
+    });
+  }
+
   filterBreeds(breeds) {
     const choiceBreeds = {
       hound: "hound",
@@ -17,8 +38,27 @@ class Breeds extends Component {
       poodle: "poodle",
       setter: "setter"
     }
+    return (choiceBreeds[breeds] !== undefined);
 
-    return breeds.filter(breed => choiceBreeds[breed]);
+
+  }
+  render() {
+    return (
+    <div>
+      <div>
+      <img src={this.state.imgUrl}/>
+      </div>
+      <div>
+    { this.state.breeds.map( (e,i)=>{
+      if(this.filterBreeds(e)){
+        return <div key={i}> {e} </div>
+      }
+    })
+    }
+      </div>
+    </div>
+
+    );
   }
 }
 
