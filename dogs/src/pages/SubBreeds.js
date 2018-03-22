20e4ea7c46b86b0f24772d72eb97216426543e70
Edson Hutton <SUPATURBOLOCITY@users.noreply.github.com>
2018-03-22 18:19:14 -0400

    Almost Complete

--- dogs/src/pages/SubBreeds.js
+++ dogs/src/pages/SubBreeds.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Options } from '../templates';
+import { Link } from 'react-router-dom';
 
 class SubBreeds extends Component {
   state = {
@@ -8,6 +9,24 @@ class SubBreeds extends Component {
     imgLabel: "",
     imgUrl: ""
   }
+
+render() {
+  console.log(this.state.subBreeds)
+  return(
+    <div>
+      <Options title={`${this.props.match.params.breed} SubBreeds`} list={this.state.subBreeds.map(breed => { return {label: breed, path: `/subbreeds/${breed}`}})} />
+      <Link to="/">Return Home</Link>
+    </div>
+  )
+};
+
+componentDidMount() {
+  axios.get(`https://dog.ceo/api/breed/${this.props.match.params.breed}/list`)
+  .then(response => {
+    this.setState( { subBreeds: response.data.message });
+  })
+  .catch(error => console.log(`You dun goofed: ${error}`))
+}
 }
 
 export default SubBreeds;
